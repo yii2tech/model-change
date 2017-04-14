e51b29b6f6969208e4984962736948f3f1afe7af
Klimov Paul <klimov.paul@gmail.com>
2017-04-14 16:24:02 +0300

    compatibility with PHPUnit < 6.x

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -13,3 +13,5 @@ require_once(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
 
 Yii::setAlias('@yii2tech/tests/unit/modelchange', __DIR__);
 Yii::setAlias('@yii2tech/modelchange', dirname(__DIR__));
+
+require_once(__DIR__ . '/compatibility.php');
